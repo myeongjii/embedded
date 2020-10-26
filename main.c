44b0d1851825c88c8cede218e26ec029d322f29f
myeongjii <72706228+myeongjii@users.noreply.github.com>
2020-10-26 19:39:55 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main(void)
 {
-return 100;
+  //Git hub는 바로 수정 가능
+   
+  //Modified
+  printf("Woww\r\n");
+  
+  printf("ABCDEFG\r\n");
+  printf("HIJKLMNOP\r\n");
+  return 1;
 }
